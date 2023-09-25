e6505580243c9b9e4dd4f9d8e1d790950dd5932e
Edson Luiz <100296179+EdsonLuizz@users.noreply.github.com>
2023-09-24 21:04:44 -0300

    Add files via upload

---
+++ ex013.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ex013 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Numero de alunos: ");
+        int n = teclado.nextInt();
+        int media = 0;
+        int notas = 0;
+        for(int i = 0; i < n; i++){
+            System.out.println("Nota: ");
+            notas = teclado.nextInt();
+            media += notas;
+        }
+        media /= n;
+        System.out.println("Media: " + media);
+    }
+}
